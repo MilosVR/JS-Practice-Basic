d736f33e6f152c2f21bd1f45375c51ba094df171
MilosVR <36715985+MilosVR@users.noreply.github.com>
2019-01-13 23:06:24 +0100

    promise basix syntax save

--- promise.js
+++ promise.js
@@ -1,1 +1,17 @@
-console.log('hello');
+////////////////////////// PROMISE SEIMPLE SYNTAX //////////////////////////////
+let prom = new Promise((resolve, reject) => {
+  let x = 5;
+  resolve(x)
+})
+prom.then(res => console.log(res))
+
+////////////////////PROMISE EXAMPLE WITH SET TIMEOUT//////////////////
+let prom1 = new Promise((resolve, reject)=>{
+  let y = 5;
+  setTimeout(() => {
+    resolve(y)
+  }, 2000);
+})
+prom1.then((res)=>{
+  console.log(res);
+})
